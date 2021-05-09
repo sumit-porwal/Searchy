fd52fdbb17e5ac1dcb8a13d413fe9cd0877ce8b9
Sumit <sumitporwal.com@gmail.com>
2021-05-09 17:02:00 +0530

    refactoring

--- spider.js
+++ spider.js
@@ -1,6 +1,27 @@
-const axios = require('axios');
 const cheerio = require('cheerio');
+const axios = require('axios');
 
-axios.get(
-	
-)
+function crawl(){
+	let metaData,title,body;
+	let result = axios(
+	 	{
+		 method:'get',
+		 url:'https://google.com',
+		 responseType: 'html'
+	 	}
+	).then((res)=>{
+		const $ = cheerio.load(res.data);
+		title = $('title').text();
+		body = $('body').text();
+		
+		metaData = $('meta').map((i,el)=>
+		$(el).attr('content')
+		);
+		
+		return {metaData,title,body}
+		
+	}).catch(err=>console.error(err));
+	return result;
+}
+
+crawl().then(re=>console.log(re));
